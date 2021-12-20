731e45c234214496c76728d25bd1da1240de8da5
Mateusz Ficek <33521251+mateuszFicek@users.noreply.github.com>
2021-12-20 11:21:29 +0100

    Vehicle documentation

--- src/main/java/Model/Vehicles/Vehicle.java
+++ src/main/java/Model/Vehicles/Vehicle.java
@@ -3,16 +3,38 @@ package Model.Vehicles;
 import Model.Highway.Cell;
 
 public class Vehicle {
+    /// Neighbourhood
     public Cell[][] neighbourhood;
+    
+    /// Maximum velocity car can reach
     public int maxVelocity;
+    
+    /// Car length
+    public int carLength;
+    
+    /// Indicates if car has entered highway
     public boolean hasEntered = false;
+    
+    /// Indicates if car has been disabled
     public boolean disabled = false;
+    
+    /// Number of exits that car should pass before exiting highway
     public int numberOfExits;
-    public int numberOfCellsToOvertake = 0;
+    
+    /// Car velocity
     int velocity;
+    
+    /// Distance to car in front
     private int distanceToNextCarInFront = 0;
+    
+    /// Distance to car behind
     private int distanceToNextCarInBack = 0;
+    
+    /// Indicates if car has changed lane
     private boolean hasChangedLane = false;
+    
+    /// Number of cells that car will try to pass other car
+    /// before yielding.
     private int numberOfCellsToPass = 40;
 
     Vehicle(int maxVelocity) {
