066bf0d309742ca1acb27bf43302f19bf17eb655
Mateusz Ficek <33521251+mateuszFicek@users.noreply.github.com>
2021-12-20 11:27:33 +0100

    Documentation for Highway

--- src/main/java/Model/Highway/Highway.java
+++ src/main/java/Model/Highway/Highway.java
@@ -8,12 +8,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Highway {
+    /// Initiate empty list of number of cars per segment
     public static List<Integer> carsOnSegment = new ArrayList<>(Collections.nCopies(17, 0));
+    
+    /// Initiate lengths of segments
     public static List<Integer> segmentsLen = Arrays.asList(191, 434, 371, 481, 611, 271, 776, 454, 345, 413, 532, 612, 666, 626, 665, 306, 599);
+    
+    /// Empty list of segment start cell numbers
     public static ArrayList<Integer> startOfSegments = new ArrayList<>(Collections.nCopies(17, 0));
+    
+    /// List of segment names
     public static String[] segmentsNames = {"Balice", "Balice2", "Modlniczka", "Modlnica", "Zielonki", "Węgrzce", "Kr. Mistrzejowice", "Kr. Grębałów", "Kr. Nowa Huta", "Kr. Przewóz", "Kr. Bieżanów", "Kr. Wieliczka", "Kr. Łagiewniki", "Kr. Południe", "Kr. Skawina", "Kr. Tyniec", "Kr. Bielany"};
     static ArrayList<Integer> segmentsByCell = new ArrayList<>(8353);
+    
+    /// Roads on highway
     public Road[] roads;
+    
+    /// Number of all segments
     private int numberOfSegments = 17;
 
     public Highway(int highwayWidth) {
